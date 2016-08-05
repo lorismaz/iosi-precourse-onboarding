38bb9cd2de7da6926ae09ca868a209ead2333777
Loris Mazloum <loris.mazloum@gmail.com>
2016-08-05 15:24:46 -0700

    Getting Started - Build a basic UI

---
+++ 02-onboarding-task/loris-mazloum-swift-tutorial/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Loris Mazloum on 8/5/16.
+//  Copyright © 2016 Loris Mazloum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
