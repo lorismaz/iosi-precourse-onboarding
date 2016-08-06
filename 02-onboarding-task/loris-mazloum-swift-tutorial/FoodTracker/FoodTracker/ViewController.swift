a1b506a4dafbf92ce237aac07171d4f0d38b2750
Loris Mazloum <loris.mazloum@gmail.com>
2016-08-05 23:01:37 -0700

    Implement a Custom Control

--- 02-onboarding-task/loris-mazloum-swift-tutorial/FoodTracker/FoodTracker/ViewController.swift
+++ 02-onboarding-task/loris-mazloum-swift-tutorial/FoodTracker/FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     // MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -58,10 +59,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         
         presentViewController(imagePickerController, animated: true, completion: nil)
-    }
-    
-    @IBAction func setDefaultLabelText(sender: UIButton) {
-        mealNameLabel.text = "Default Text"
     }
     
     override func viewDidLoad() {
