f4cf42ce02077405fa09a16a74e1c19772b57ce3
Loris Mazloum <loris.mazloum@gmail.com>
2016-08-07 17:52:37 -0700

    Create a Table View

--- 02-onboarding-task/loris-mazloum-swift-tutorial/FoodTracker/FoodTracker/MealViewController.swift
+++ 02-onboarding-task/loris-mazloum-swift-tutorial/FoodTracker/FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Loris Mazloum on 8/5/16.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
-    @IBOutlet weak var ratingControl: RatingControl!
     
     // MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -24,7 +22,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        mealNameLabel.text = textField.text
+        //mealNameLabel.text = textField.text
     }
     
     // MARK: UIImagePickerControllerDelegate
